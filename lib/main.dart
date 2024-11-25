22b9d34a17dac18d6d0d4f04b6666c8e42dc3bd2
ryouji.saitou.ay <ryouji.saitou.ay@nttdocomo.com>
2024-11-25 07:16:36 +0000

    Argumentの練習

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,59 @@ class MyApp extends StatelessWidget {
           title: const Text(appTitle),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          child: Text('Title sectionです'),
         ),
+      ),
+    );
+  }
+}
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  location,
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: const Color.fromARGB(255, 95, 54, 244),
+          ),
+          const Text('41'),
+        ],
       ),
     );
   }
